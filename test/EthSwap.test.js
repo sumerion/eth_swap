e13c8cd80af7c8ea7ec34747670aa38738334bfd
Kris Barnhoorn <kris@sumerion.com>
2020-08-23 17:10:24 +0200

    Refactored test

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -6,10 +6,17 @@ require('chai')
   .should()
 
 contract('EthSwap', (accounts) => {
+  let token;
+  let ethSwap;
+
+  before(async() => {
+    token = await Token.new();
+    ethSwap = await EthSwap.new();
+    await token.transfer(ethSwap.address, '1000000000000000000000000');
+  });
 
   describe('Token deployment', async () => {
     it('should have a contract name', async() => {
-      let token = await Token.new();
       const name = await token.name();
       assert.equal(name, 'DApp Token');
     })
@@ -17,15 +24,11 @@ contract('EthSwap', (accounts) => {
 
   describe('EthSwap deployment', async () => {
     it('should have a contract name', async() => {
-      let ethSwap = await EthSwap.new();
       const name = await ethSwap.name();
       assert.equal(name, 'EthSwap Instant Exchange');
     })
 
     it('should have all the tokens', async() => {
-      let token = await Token.new();
-      let ethSwap = await EthSwap.new();
-      await token.transfer(ethSwap.address, '1000000000000000000000000');
       const balance = await token.balanceOf(ethSwap.address);
       assert.equal(balance.toString(), '1000000000000000000000000');
     })
